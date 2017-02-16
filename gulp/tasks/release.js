b3fe4ac0b2f34a3020a9832b5f5481e2868d48f6
Ian Page Hands <iphands@gmail.com>
2017-02-16 10:49:54 -0500

    [gulp] Testing git break on dirty

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -4,12 +4,25 @@
 const config = require('../config');
 const gulp = require('gulp');
 const runSequence = require('run-sequence');
+const git = require('gulp-git');
 
 function buildAndRelease(cb, bump) {
-    cb = cb || function () {};
-    global.isProd = true;
-    global.isRelease = true;
-    runSequence(bump, cb);
+    git.status({args: '--porcelain'}, function (err, stdout) {
+        if (err) throw err;
+
+        if (stdout !== '') {
+            throw new Error(`Refusing to do a gulp release!
+This release task does a Git commit and tag.
+Please stash or commit everything before you continue.
+We want the release commits to not include any additional changes.
+`);
+        }
+
+        cb = cb || function () {};
+        global.isProd = true;
+        global.isRelease = true;
+        runSequence(bump, cb);
+    });
 }
 
 gulp.task('release', ['releasepatch']);
