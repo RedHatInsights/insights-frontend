75628bf2174cf281698ae2875a1fd00dec2635a6
Ian Page Hands <iphands@gmail.com>
2017-02-16 16:00:02 -0500

    [jenkins] Kick the branch indexing not the job
    The job might not actually exist in Jenkins yet. Kicking the branch indexing
    should find the new branch and start the job anyway.

--- gulp/tasks/jenkins.js
+++ gulp/tasks/jenkins.js
@@ -5,33 +5,35 @@
 const gulp = require('gulp');
 const git = require('gulp-git');
 const got = require('got');
-const token = 'notasecret';
 const deployTokenAuth = 'ihands:0c2928006651ef78558b248cc7972d80';
-const baseUrl = 'https://insights-jenkins.rhev-ci-vms.eng.rdu2.redhat.com'
-      + '/job/insights-frontend/job';
+const jenkinsUrl = 'https://insights-jenkins.rhev-ci-vms.eng.rdu2.redhat.com';
+const jobBaseUrl = `${jenkinsUrl}/job/insights-frontend/job`;
+const branchIndexingUrl = `${jenkinsUrl}/job/insights-frontend/build?delay=0`;
 const priv = {};
 
 priv.encode = function getBranchUrlComponent(branch) {
     return encodeURIComponent(encodeURIComponent(branch));
 };
 
+https://insights-jenkins.rhev-ci-vms.eng.rdu2.redhat.com/job/insights-frontend/build?delay=0
+
 gulp.task('kick', function () {
     git.revParse({args: '--abbrev-ref HEAD'}, function (err, branch) {
-        const url = `${baseUrl}/${priv.encode(branch)}`;
+        const jobUrl = `${jobBaseUrl}/${priv.encode(branch)}`;
 
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
         console.log(`Kicking build for ${branch}`);
 
-        got.post(`${url}/build?token${token}&delay=0sec`, {
+        got.post(branchIndexingUrl, {
+            followRedirect: false,
             auth: deployTokenAuth
         }).then(res => {
             console.log(`Response code: ${res.statusCode}`);
-            console.log(`Success, visit ${url} for details`);
+            console.log(`Success, visit ${jobUrl} for details`);
         }).catch(error => {
             console.log(error);
             throw error;
         });
     });
 });
-
