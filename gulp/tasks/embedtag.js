3a2ac5046aae5610bc29f6f9d305fa6324b9b999
Ian Page Hands <iphands@gmail.com>
2017-02-21 10:50:23 -0500

    [gulp] dont auto call embedtag. Added embedhash

--- gulp/tasks/embedtag.js
+++ gulp/tasks/embedtag.js
@@ -1,15 +1,26 @@
+/*global require*/
 'use strict';
 
-var config = require('../config');
-var gulp = require('gulp');
-var replace = require('gulp-replace');
+const config = require('../config');
+const gulp = require('gulp');
+const replace = require('gulp-replace');
+const git = require('gulp-git');
+const priv = {};
 
-// Revision files
-gulp.task('embedtag', function () {
-
-    // Replace ___TAG___ with the current tag
+priv.changeTagString = function changeTagString(newString) {
     return gulp.src(config.dist.root + '/index.html')
-        .pipe(replace('___TAG___', config.pkg.version))
+        .pipe(replace('___TAG___', newString))
         .pipe(gulp.dest(config.dist.root));
+};
 
+gulp.task('embedtag', function () {
+    return priv.changeTagString(config.pkg.version);
 });
+
+gulp.task('embedhash', function () {
+    git.revParse({args:' HEAD'}, function (err, hash) {
+        return priv.changeTagString(hash);
+    });
+});
+
+
--- gulp/tasks/production.js
+++ gulp/tasks/production.js
@@ -1,12 +1,11 @@
+/*global require, global*/
 'use strict';
 
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('prod', function (cb) {
-
     cb = cb || function () {};
     global.isProd = true;
-    runSequence('clean-all', 'views', 'translations', ['copy.translations', 'static', 'styles', 'images', 'fonts', 'browserify', 'openshift'], 'rev', 'embedtag', cb);
-
+    runSequence('clean-all', 'views', 'translations', ['copy.translations', 'static', 'styles', 'images', 'fonts', 'browserify', 'openshift'], 'rev', cb);
 });
