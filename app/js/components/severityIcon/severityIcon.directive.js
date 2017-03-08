989c54abb881aeedeeed61846a7baf22191ba2aa
Jeremy Crafts <jcrafts@redhat.com>
2017-03-08 13:30:29 -0500

    add logic to display new icons, start propagating to pages

--- app/js/components/severityIcon/severityIcon.directive.js
+++ app/js/components/severityIcon/severityIcon.directive.js
@@ -5,36 +5,55 @@ var componentsModule = require('../');
 /**
  * @ngInject
  */
-function severityIconCtrl($scope, gettextCatalog) {
-    $scope.$watch('severity', function (severity) {
-        if (severity === 'INFO') {
-            $scope.iconClass = 'i-info fa-info-circle';
-            $scope.labelClass = 'i-info';
-            $scope.labelValue = gettextCatalog.getString('Low');
-        } else if (severity === 'WARN') {
-            $scope.iconClass = 'i-warning fa-warning';
-            $scope.labelClass = 'i-warning';
-            $scope.labelValue = gettextCatalog.getString('Medium');
-        } else if (severity === 'ERROR') {
-            $scope.iconClass = 'i-error fa-times-circle';
-            $scope.labelClass = 'i-error';
-            $scope.labelValue = gettextCatalog.getString('High');
-        } else if (severity === 'CRITICAL') {
-            $scope.iconClass = 'fa-bomb';
-            $scope.labelClass = '';
-            $scope.labelValue = gettextCatalog.getString('Critical');
-        } else {
-            $scope.iconClass = '';
-            $scope.labelClass = '';
+function severityIconCtrl($scope) {
+    function icon (level) {
+        switch (level) {
+            case 1:
+                return '../images/i_low.svg';
+            case 2:
+                return '../images/i_med.svg';
+            case 3:
+                return '../images/i_high.svg';
+            case 4:
+                return '../images/i_critical.svg';
+            default:
+                return '';
         }
-    });
+    }
+
+    $scope.impactIcon = function () {
+        return icon($scope.rule.rec_impact);
+    };
+
+    $scope.probabilityIcon = function () {
+        return icon($scope.rule.rec_likelihood);
+    };
+
+    $scope.riskIcon = function () {
+        if (typeof $scope.rule.severity !== 'number') {
+
+            // map the strings to sev #s til #s are in place
+            return {
+                INFO: icon(1),
+                WARN: icon(2),
+                ERROR: icon(3),
+                CRITICAL: icon(4)
+            }[$scope.rule.severity];
+        }
+
+        return icon($scope.rule.severity);
+    };
 }
 
-function severityIcon() {
+function severityIcon () {
     return {
         scope: {
-            severity: '=',
-            label: '='
+            rule: '=',
+            label: '=',
+            impactOnly: '=',
+            probabilityOnly: '=',
+            riskOnly: '=',
+            noLabels: '='
         },
         templateUrl: 'js/components/severityIcon/severityIcon.html',
         restrict: 'EC',
