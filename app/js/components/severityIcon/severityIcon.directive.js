8ce769dcf23e6382949cd31ac839d6973bcf849d
Ian Page Hands <iphands@gmail.com>
2017-03-20 15:05:51 -0400

    lint

--- app/js/components/severityIcon/severityIcon.directive.js
+++ app/js/components/severityIcon/severityIcon.directive.js
@@ -31,6 +31,7 @@ function severityIconCtrl($scope, gettextCatalog) {
             if (has(priv.typeMap, $scope.type)) {
                 return;
             }
+
             throw new Error('Invalid severity-icon type selected! ' + $scope.type);
         }
     };
