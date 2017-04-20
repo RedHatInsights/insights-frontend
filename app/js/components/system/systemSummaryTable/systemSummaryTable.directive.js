0fe7645eeaebc0064c848626dbbf68011d207af2
Ian Page Hands <iphands@gmail.com>
2017-04-20 15:06:45 -0400

    [lodash] update isString

--- app/js/components/system/systemSummaryTable/systemSummaryTable.directive.js
+++ app/js/components/system/systemSummaryTable/systemSummaryTable.directive.js
@@ -4,7 +4,7 @@ var componentsModule = require('../../');
 
 var filter = require('lodash/filter');
 var findWhere = require('lodash/find');
-var isString = require('lodash/lang/isString');
+var isString = require('lodash/isString');
 var isEmpty = require('lodash/isEmpty');
 var sortby = require('lodash/collection/sortBy');
 
