482f48cf010d6aac1b3ff364502e0a0a5dda46ba
Jeremy Crafts <gravitypriest@users.noreply.github.com>
2017-04-24 10:50:35 -0400

    task to list Insights components and their occurrences (#62)
    * task to list Insights components and their occurrences
    
    * report components never used by first getting all the components in our lib

---
+++ gulp/tasks/componentList.js
@@ -0,0 +1,24 @@
+/*global require*/
+'use strict';
+
+var gulp = require('gulp');
+var spawn = require('child_process').spawn;
+
+gulp.task('component-list', function (done) {
+	var proc = spawn('python', ['scripts/component_list.py']);
+	var output = '';
+	var err = '';
+	proc.stdout.on('data', function(data) {
+		output += data.toString();
+	});
+	proc.stderr.on('data', function(data) {
+		err += data.toString();
+	});
+	proc.on('close', function() {
+		console.log(output);
+		if (err) {
+			console.log(err);
+		}
+		done();
+	});
+});
