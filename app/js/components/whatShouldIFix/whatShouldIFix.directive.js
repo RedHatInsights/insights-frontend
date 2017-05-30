4bba5f66b031b86bd42fce0991b2756d9d122798
Jeremy Crafts <jcrafts@redhat.com>
2017-05-30 12:46:46 -0400

    update for new lodash functions

--- app/js/components/whatShouldIFix/whatShouldIFix.directive.js
+++ app/js/components/whatShouldIFix/whatShouldIFix.directive.js
@@ -1,34 +1,28 @@
 'use strict';
 
 const componentsModule = require('../');
-const sortBy = require('lodash/collection/sortBy');
-const takeRight = require('lodash/array/takeRight');
+const orderBy = require('lodash/orderBy');
+const map = require('lodash/map');
 
 /**
  * @ngInject
  */
 function whatShouldIFixCtrl($scope, Rule, Severities) {
-
-    function padZeroes (num) {
-
-        // use this hack like in digests til we have _.orderBy
-        var padded = '0000000000' + num;
-        padded = padded.substr(('' + num).length);
-        return padded;
-    }
-
     Rule.getRulesLatest()
     .then(function (res) {
         var rules = res.data.resources;
-        $scope.rules = sortBy(rules, function (r) {
-            return [r.rec_likelihood,
-                    Severities.indexOf(r.severity),
-                    padZeroes(r.report_count),
-                    r.ansible];
+
+        // need numerical values for severities for sorting below
+        rules = map(rules, function (r) {
+            r.severityIdx = Severities.indexOf(r.severity);
+            return r;
         });
 
-        $scope.rules = takeRight($scope.rules, 5);
-        $scope.rules.reverse();
+        $scope.rules = orderBy(
+            rules,
+            ['rec_likelihood', 'severityIdx', 'report_count', 'ansible'],
+            ['desc', 'desc', 'desc', 'desc']
+        ).slice(0, 5);
     });
 }
 
