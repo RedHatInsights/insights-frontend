jharting/insights-frontend-1

adds search and incidents filter to actions page 2

app/js/components/topics/topicRuleFilters/topicRuleFilters.controller.js
@@ -1,0 +1,27 @@
+'use strict';
+
+var componentsModule = require('../../');
+
+/**
+ * @ngInject
+ */
+function topicRuleFiltersCtrl($scope, FilterService, Events, Group) {
+
+    $scope.resetFilters = function () {
+        FilterService.clearAll();
+        Group.setCurrent({});
+        $scope.$emit('group:change', {});
+        $scope.$broadcast(Events.filters.reset);
+    };
+}
+
+function topicRuleFilters() {
+    return {
+        templateUrl: 'js/components/topics/topicRuleFilters/topicRuleFilters.html',
+        restrict: 'E',
+        replace: true,
+        controller: topicRuleFiltersCtrl
+    };
+}
+
+componentsModule.directive('topicRuleFilters', topicRuleFilters);

RedHatInsights/insights-frontend

app/js/components/topics/topicRuleFilters/topicRuleFilters.controller.js
@@ -1,0 +1,26 @@
+'use strict';
+
+var componentsModule = require('../../');
+
+/**
+ * @ngInject
+ */
+function topicRuleFiltersCtrl($scope, FilterService, Events, Group) {
+
+    $scope.resetFilters = function () {
+        Group.setCurrent({});
+        $scope.$emit('group:change', {});
+        $scope.$broadcast(Events.topicFilters.reset);
+    };
+}
+
+function topicRuleFilters() {
+    return {
+        templateUrl: 'js/components/topics/topicRuleFilters/topicRuleFilters.html',
+        restrict: 'E',
+        replace: true,
+        controller: topicRuleFiltersCtrl
+    };
+}
+
+componentsModule.directive('topicRuleFilters', topicRuleFilters);