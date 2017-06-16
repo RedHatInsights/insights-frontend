0dccac703e67da4aab2fc632d47ef00936dc01dd
Ian Page Hands <iphands@gmail.com>
2017-06-16 13:38:32 -0400

    update run.sh

--- smoketest/run.sh
+++ smoketest/run.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 export TEST_TRY_NUM=$1
-time casperjs \
-     --engine=phantomjs \
-     --verbose \
-     --log-level=debug \
-     --load-images=true \
-     --local-to-remote-url-access=true \
-     --web-security=false --ssl-ciphers=all \
-     --ssl-protocol=any \
-     --ignore-ssl-errors=true \
-     test smoketest/index.js
-
+DEBUG=nightmare:act* mocha smoketest/index.js
