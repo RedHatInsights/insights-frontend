4ff5d5408f4359062a24a2f3ebb5b3cf14017994
Ian Page Hands <iphands@gmail.com>
2017-06-19 17:47:13 -0400

    tmp

--- smoketest/index.js
+++ smoketest/index.js
@@ -13,14 +13,9 @@ nightmare.on('console', (log, msg) => {
     console.log(`[browser] ${msg}`);
 });
 
-nightmare.on('did-stop-loading', function () {
-    nightmare.url((link) => {
-        console.log(link);
-    });
-});
-
 describe('Insights Portal Smoke Test', function () {
     this.timeout(45 * 1000);
+
     after((done) => {
         nightmare.end(done);
     });
@@ -28,18 +23,13 @@ describe('Insights Portal Smoke Test', function () {
     describe('Setup', () => {
         it('should be able to "Go To Application" and login', () => {
             return nightmare
-                .wrap('click go to app', (n) => {
-                    n.goto(funcs.getUrl())
-                        .click(el.goToApp);
-                })
-                .wrap('fill out login form, and submit', (n) => {
-                    n.wait(() => { return window.location.href.match(/https:\/\/.*?(\/auth)/); })
-                        .wait(el.loginFormUsername)
-                        .insert(el.loginFormUsername, env.TEST_USERNAME)
-                        .insert(el.loginFormPassword, env.TEST_PASSWORD)
-                        .waitAndClick(el.loginFormSubmit);
-                })
-                .wait(el.overview.severitySummary);
+                .goto(funcs.getUrl())
+                .click(el.goToApp)
+                .wait(el.loginFormUsername)
+                .insert(el.loginFormUsername, env.TEST_USERNAME)
+                .insert(el.loginFormPassword, env.TEST_PASSWORD)
+                .waitAndClick(el.loginFormSubmit)
+                .waitAll('nav');
         });
     });
 
--- smoketest/suites/actions.js
+++ smoketest/suites/actions.js
@@ -6,50 +6,41 @@ module.exports = (nightmare) => {
     describe('Actions', () => {
         it('should be able to get to the System Modal through Actions', (done) => {
             nightmare
-                .wrap('page one', (n) => {
-                    n.waitAndClick(el.nav.actions)
-                        .waitAll('nav')
-                        .waitAll('actions.page1');
-                })
+                .waitAndClick(el.nav.actions)
+                .waitAll('nav')
+                .waitAll('actions.page1')
 
-                .wrap('page two', (n) => {
-                    n.waitAndClick(el.actions.page1.legend_security)
-                        .waitAll('nav')
-                        .waitAll('actions.page2');
-                })
+                .waitAndClick(el.actions.page1.legend_security)
+                .waitAll('nav')
+                .waitAll('actions.page2')
 
-                .wrap('page one', (n) => {
-                    n.waitAndClick(el.actions.page2.firstRuleInTable)
-                        .waitAll('nav')
-                        .waitAll('actions.page3')
-                        .evaluate((el) => {
-                            return {
-                                ruleNamePage3: document.querySelector(el.actions.page3.ruleTitle).textContent,
-                                systemNamePage3: document.querySelector(el.actions.page3.firstSystemInTable).textContent
-                            };
-                        }, el);
-                })
+
+                .waitAndClick(el.actions.page2.firstRuleInTable)
+                .waitAll('nav')
+                .waitAll('actions.page3')
+                .evaluate((el) => {
+                    return {
+                        ruleNamePage3: document.querySelector(el.actions.page3.ruleTitle).textContent,
+                        systemNamePage3: document.querySelector(el.actions.page3.firstSystemInTable).textContent
+                    };
+                }, el)
                 .then((stash) => {
                     nightmare
-                        .wrap('page three', (n) => {
-                            n.waitAndClick(el.actions.page3.firstSystemInTable)
-                                .waitAll('nav')
-                                .waitAll('systemModal')
-                                .evaluate((el, stash) => {
-                                    stash.ruleNameModal = document.querySelector(el.systemModal.firstRule).textContent.trim();
-                                    stash.systemNameModal =  document.querySelector(el.systemModal.hostname).textContent.trim();
-                                    return stash;
-                                }, el, stash);
-                        })
+                        .waitAndClick(el.actions.page3.firstSystemInTable)
+                        .waitAll('nav')
+                        .waitAll('systemModal')
+                        .evaluate((el, stash) => {
+                            stash.ruleNameModal = document.querySelector(el.systemModal.firstRule).textContent.trim();
+                            stash.systemNameModal =  document.querySelector(el.systemModal.hostname).textContent.trim();
+                            return stash;
+                        }, el, stash)
                         .then((stash) => {
-                            nightmare.wrap('system modal', (n) => {
-                                stash.ruleNameModal.should.equal(`Security > ${stash.ruleNamePage3}`);
-                                stash.systemNamePage3.should.equal(stash.systemNameModal);
-                                n.waitAndClick(el.systemModal.exButton)
-                                    .waitAll('nav')
-                                    .then(done)
-                                    .catch(done);
-                            });
+                            stash.ruleNameModal.should.equal(`Security > ${stash.ruleNamePage3}`);
+                            stash.systemNamePage3.should.equal(stash.systemNameModal);
+                            nightmare.waitAndClick(el.systemModal.exButton)
+                                .waitAll('nav')
+                                .then(done)
+                                .catch(done);
                         }).catch(done);
                 }).catch(done);
         });
