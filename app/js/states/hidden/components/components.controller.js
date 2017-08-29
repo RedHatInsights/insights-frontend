4cff7939008da7fecbe0096bf8ab584abb94a099
Ryan Long <rlong@redhat.com>
2017-08-29 10:11:49 -0400

    Added more material implementation

--- app/js/states/hidden/components/components.controller.js
+++ app/js/states/hidden/components/components.controller.js
@@ -3,9 +3,16 @@
 
 var statesModule = require('../../');
 
-/**
- * @ngInject
- */
-function ComponentsCtrl() {}
+const mdc = require('material-components-web/dist/material-components-web');
+
+//const mdcTextField = require('@material/textfield');
+
+function ComponentsCtrl() {
+    mdc.textfield.MDCTextfield.attachTo(document.querySelector('.mdc-textfield'));
+    mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-icon-toggle'));
+    mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button'));
+    mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-fab'));
+    mdc.autoInit();
+}
 
 statesModule.controller('ComponentsCtrl', ComponentsCtrl);
