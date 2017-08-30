1cfc527e972d45ed3495fcaa5498d43475d8a148
Ryan Long <rlong@redhat.com>
2017-08-30 08:08:37 -0400

    Updated material to angular

--- app/js/insights.js
+++ app/js/insights.js
@@ -21,6 +21,8 @@ require('angular-gravatar');
 require('ng-table');
 require('angular-datepicker');
 require('ng-infinite-scroll');
+require('angular-aria');
+require('angular-material');
 
 require('./components/ui-bootstrap-custom');
 
--- app/js/states/hidden/components/components.controller.js
+++ app/js/states/hidden/components/components.controller.js
@@ -3,16 +3,7 @@
 
 var statesModule = require('../../');
 
-const mdc = require('material-components-web/dist/material-components-web');
-
-//const mdcTextField = require('@material/textfield');
-
 function ComponentsCtrl() {
-    mdc.textfield.MDCTextfield.attachTo(document.querySelector('.mdc-textfield'));
-    mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-icon-toggle'));
-    mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-button'));
-    mdc.ripple.MDCRipple.attachTo(document.querySelector('.mdc-fab'));
-    mdc.autoInit();
 }
 
 statesModule.controller('ComponentsCtrl', ComponentsCtrl);
