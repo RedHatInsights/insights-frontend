13cd47805db6618f1d75bbdce198c05233519025
Ryan Long <rlong@redhat.com>
2017-08-30 14:48:03 -0400

    added demo page in hidden/components

--- app/js/states/hidden/components/components.controller.js
+++ app/js/states/hidden/components/components.controller.js
@@ -3,7 +3,24 @@
 
 var statesModule = require('../../');
 
-function ComponentsCtrl() {
+function ComponentsCtrl(
+    $scope,
+    $mdDialog) {
+    $scope.status = '  ';
+    $scope.customFullscreen = false;
+
+    $scope.showAlert = function (ev) {
+        $mdDialog.show(
+            $mdDialog.alert()
+            .parent(angular.element(document.querySelector('#popupContainer')))
+            .clickOutsideToClose(true)
+            .title('This is an alert title')
+            .textContent('Wow!!! Look at this beautiful dialog!!!')
+            .ariaLabel('Alert Dialog Demo')
+            .ok('Got it!')
+            .targetEvent(ev)
+        );
+    };
 }
 
 statesModule.controller('ComponentsCtrl', ComponentsCtrl);
