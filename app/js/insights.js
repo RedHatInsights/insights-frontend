528ed92ade57ae21b0caac8f138e17927677179e
Ryan Long <rlong@redhat.com>
2017-09-01 10:14:52 -0400

    changed package.json and updated demo with insights theme

--- app/js/insights.js
+++ app/js/insights.js
@@ -111,3 +111,86 @@ angular.module('ui.select').run(function ($animate) {
         return overridden(elem);
     };
 });
+
+// Angular Material Theme
+angular.module('ngMaterial')
+.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+    .primaryPalette('blue')
+    .accentPalette('grey')
+    .warnPalette('red');
+});
+
+// Insights Material Theme
+angular.module('ngMaterial')
+.config(function ($mdThemingProvider) {
+
+    $mdThemingProvider.definePalette('insightsPrimary', {
+        50: '90cae7',
+        100: '7bc0e2',
+        200: '66b6de',
+        300: '51acd9',
+        400: '3ca2d5',
+        500: '2c96cb',
+        600: '2787b6',
+        700: '2377a1',
+        800: '1e688c',
+        900: '1a5877',
+        A100: 'a5d4eb',
+        A200: 'badef0',
+        A400: 'cfe8f4',
+        A700: '154962',
+        contrastDefaultColor: 'light',
+        contrastDarkColors:
+        ['50', '100', '200', '300', '400', 'A100'],
+        contrastLightColors: undefined
+    });
+
+    $mdThemingProvider.definePalette('insightsAccent', {
+        50: '8c8c8c',
+        100: '999999',
+        200: 'a6a6a6',
+        300: 'b3b3b3',
+        400: 'bfbfbf',
+        500: 'cccccc',
+        600: 'e6e6e6',
+        700: 'f2f2f2',
+        800: 'ffffff',
+        900: 'ffffff',
+        A100: 'e6e6e6',
+        A200: 'd9d9d9',
+        A400: 'cccccc',
+        A700: 'ffffff',
+        contrastDefaultColor: 'light',
+        contrastDarkColors:
+        ['50', '100', '200', '300', '400', 'A100'],
+        contrastLightColors: undefined
+    });
+
+    $mdThemingProvider.definePalette('insightsWarn', {
+        50: 'f0b9b8',
+        100: 'eba5a3',
+        200: 'e7908e',
+        300: 'e27c79',
+        400: 'de6764',
+        500: 'd9534f',
+        600: 'd43e3a',
+        700: 'c9302c',
+        800: 'b42b27',
+        900: 'a02622',
+        A100: 'f4cecd',
+        A200: 'f9e2e2',
+        A400: 'fdf7f7',
+        A700: '8b211e',
+        contrastDefaultColor: 'light',
+        contrastDarkColors:
+        ['50', '100', '200', '300', '400', 'A100'],
+        contrastLightColors: undefined
+    });
+
+    $mdThemingProvider.theme('default')
+        .primaryPalette('insightsPrimary')
+        .accentPalette('insightsAccent')
+        .warnPalette('insightsWarn');
+
+});
