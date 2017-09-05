c5356208124b725f0461f60bf6dac47f54da05be
Ryan Long <rlong@redhat.com>
2017-09-05 09:49:53 -0400

    Updated material demo page with more working examples

--- app/js/states/hidden/components/components.controller.js
+++ app/js/states/hidden/components/components.controller.js
@@ -9,18 +9,39 @@ function ComponentsCtrl(
     $scope.status = '  ';
     $scope.customFullscreen = false;
 
+    $scope.myDate = new Date();
+
     $scope.showAlert = function (ev) {
         $mdDialog.show(
             $mdDialog.alert()
             .parent(angular.element(document.querySelector('#popupContainer')))
             .clickOutsideToClose(true)
-            .title('This is an alert title')
-            .textContent('Wow!!! Look at this beautiful dialog!!!')
+            .title('This is the title')
+            .textContent('This is the content')
             .ariaLabel('Alert Dialog Demo')
             .ok('Got it!')
             .targetEvent(ev)
         );
     };
+
+    $scope.showConfirm = function (ev) {
+        $mdDialog.show(
+            $mdDialog.confirm()
+            .parent(angular.element(document.querySelector('#popupContainer')))
+            .clickOutsideToClose(true)
+            .title('This is a title')
+            .textContent('This is the content')
+            .ariaLabel('Comfirm Dialog Demo')
+            .ok('Confirm')
+            .cancel('Cancel')
+            .targetEvent(ev)
+        );
+    };
+
+    $scope.onSwipeUp = function () {
+        window.alert('You swiped up!!');
+    };
+
 }
 
 statesModule.controller('ComponentsCtrl', ComponentsCtrl);
