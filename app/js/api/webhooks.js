ab04bd1f2a7decdfaf30ad77e2421814057421b6
Jozef Hartinger <jharting@redhat.com>
2017-09-06 09:57:02 +0200

    webhook management UI
    https://github.com/RedHatInsights/insights-api/issues/1

---
+++ app/js/api/webhooks.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const apiModule = require('./');
+const URI = require('urijs');
+const pick = require('lodash/pick');
+
+/**
+ * @ngInject
+ */
+function Webhooks($http, AccountService, InsightsConfig) {
+    const root = InsightsConfig.apiRoot;
+
+    function url (...segments) {
+        const url = URI(root);
+        url.segment('webhooks');
+        segments.forEach(segment => url.segment(String(segment)));
+        url.addSearch(AccountService.queryParam());
+        return url.toString();
+    }
+
+    return {
+
+        get: function () {
+            return $http.get(url());
+        },
+
+        create: function (webhook) {
+            return $http.post(url(), webhook);
+        },
+
+        ping: function () {
+            return $http.post(url('ping'));
+        },
+
+        update: function (webhook) {
+            return $http.put(url(webhook.id), pick(webhook, ['active', 'url']));
+        },
+
+        delete: function (webhook) {
+            return $http.delete(url(webhook.id));
+        }
+    };
+}
+
+apiModule.factory('Webhooks', Webhooks);
