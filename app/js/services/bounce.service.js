ba1f8c7945a557599fa1c7abc118afafd1a324b9
Ian Page Hands <iphands@gmail.com>
2017-09-07 15:04:40 -0400

    lint remove un-used function

--- app/js/services/bounce.service.js
+++ app/js/services/bounce.service.js
@@ -15,22 +15,6 @@ function BounceService(InsightsConfig, $injector) {
             }
         }
     };
-
-    // The evaluation state tracks the original state the user wanted
-    // to go. This prevents that information from being lost
-    // during a redirect.
-    function getOriginalDestination() {
-        const $state = $injector.get('$state');
-        const originalPath = $state.params.originalPath;
-        if (originalPath) {
-            return originalPath;
-        }
-
-        const redirectUri = URI(window.location.toString());
-        const basePath = redirectUri.segment()[0];
-        redirectUri.segment([basePath, 'overview']);
-        return redirectUri.toString();
-    }
 }
 
 servicesModule.service('BounceService', BounceService);
