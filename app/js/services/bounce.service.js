f9640377dc764f07d3c672d5178de8286742d15f
Ian Page Hands <iphands@gmail.com>
2017-09-07 15:05:47 -0400

    lint remove more dead stuff

--- app/js/services/bounce.service.js
+++ app/js/services/bounce.service.js
@@ -1,13 +1,13 @@
-/*global window, require*/
+/*global require*/
 'use strict';
 
 const servicesModule = require('./');
-const URI = require('urijs');
 const Jwt = require('jwt-redhat').default;
+
 /**
  * @ngInject
  */
-function BounceService(InsightsConfig, $injector) {
+function BounceService(InsightsConfig) {
     return {
         bounce: function () {
             if (InsightsConfig.authenticate) {
