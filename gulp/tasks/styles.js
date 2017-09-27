a0c43f70bfcbe341cecc4ebb0b1dcafca997f4d9
Ian Page Hands <iphands@gmail.com>
2017-09-27 13:33:20 -0400

    Remove all ruby and compass, rewrite styles gulp task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,13 +2,16 @@
 'use strict';
 
 const browserSync = require('browser-sync');
-const compass = require('gulp-compass');
-const concat = require('gulp-concat');
 const config = require('../config');
 const gulp = require('gulp');
 const gulpif = require('gulp-if');
-const handleErrors = require('../util/handleErrors');
 const replace = require('gulp-replace');
+const concat = require('gulp-concat');
+
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('make-compontents-scss', function () {
     return gulp.src(config.styles.componentsSrc)
@@ -17,31 +20,14 @@ gulp.task('make-compontents-scss', function () {
 });
 
 gulp.task('styles', ['make-compontents-scss'], function () {
-    let styles = global.isRelease ? config.styles.srcRelease : config.styles.src;
-    const vendorStyles = config.styles.vendor;
-    const outputStyle = global.isProd ? 'compressed' : 'nested';
-
-    for (let i = 0; i < vendorStyles.length; i++) {
-        vendorStyles[i] = '!' + vendorStyles[i];
-        vendorStyles[i] = vendorStyles[i].replace('styles/vendor');
-    }
-
-    styles = styles.concat(vendorStyles);
+    const styles = global.isRelease ? config.styles.srcRelease : config.styles.src;
     return gulp.src(styles)
-        .pipe(compass({
-            sass:  'app/styles',
-            css:   config.styles.temp,
-            style: outputStyle,
-            import_path: [
-                './node_modules/flexboxgrid/css/',
-                './node_modules/bootstrap-sass/vendor/assets/stylesheets/'
-            ],
-            require: ['sass-css-importer']
-        }))
-        .on('error', handleErrors)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({ browsers: "> 5%" }))
         .pipe(replace('../fonts', '../static/fonts'))
+        .pipe(gulpif(global.isRelease, cleanCSS()))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.styles.dest))
-        .pipe(gulpif(browserSync.active, browserSync.reload({
-            stream: true
-        })));
+        .pipe(gulpif(browserSync.active, browserSync.reload({ stream: true })));
 });
