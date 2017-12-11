78e363cc8e964107675aa3a602d47df5278ec649
Matt Nolting <mnolting@redhat.com>
2017-12-11 09:32:59 -0500

    Adding export csv button

--- app/js/states/base/app.controller.js
+++ app/js/states/base/app.controller.js
@@ -10,6 +10,14 @@ function AppCtrl($scope, $rootScope, User, PermissionService) {
         $rootScope.isContentManager =
             (PermissionService.has(user, PermissionService.PERMS.CONTENT_MANAGER));
     });
+
+    $scope.stateCheck = function () {
+        if ($scope.state.is("app.actions") || $scope.state.is("app.inventory") || $scope.state.is("app.actions-rule") || $scope.state.is("app.topic")) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 }
 
 statesModule.controller('AppCtrl', AppCtrl);
