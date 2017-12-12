9572777d292201d4ec857bac0f9ebb58855ef3ff
Ian Page Hands <iphands@gmail.com>
2017-12-12 13:51:37 -0500

    Lint Pedno analytics stuff

--- app/js/services/analytics.service.js
+++ app/js/services/analytics.service.js
@@ -35,14 +35,18 @@ priv.doGoogleAnalytics = function doGoogleAnalytics ($location, User, IgnoreAcco
     });
 };
 
-function pendoBlob () { (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/f210c485-387f-43ad-4eee-f55bab22507f/pendo.js';z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo') };
+// jshint ignore: start
+// jscs:disable
+priv.pendoBlob = function () { (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/f210c485-387f-43ad-4eee-f55bab22507f/pendo.js';z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo') };
+// jscs:enable
+// jshint ignore: end
 
 /**
  * @ngInject
  */
 function AnalyticsService(InsightsConfig, $location, IgnoreAccountList, User) {
     pub.initPendo = function () {
-        pendoBlob();
+        priv.pendoBlob();
         User.asyncCurrent((user) => {
             if (typeof window.pendo !== 'undefined') {
                 const pendoConf = {
