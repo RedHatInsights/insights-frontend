cb72490b5f4490519415cac646ddd18d695e4ca9
Ryan Long <rlong@redhat.com>
2018-01-09 13:09:58 -0500

    hooked up vulnerabilities tab in modal

---
+++ app/js/components/vulnerabilities/vulnerabilitiesModal/vulnerabilitiesModal.directive.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var componentsModule = require('../../');
+
+/**
+ * @ngInject
+ */
+function vulnerabilitiesModalCtrl() {
+
+}
+
+function vulnerabilitiesModal() {
+    return {
+        templateUrl: 'js/components/vulnerabilities/vulnerabilitiesModal/vulnerabilitiesModal.html',
+        restrict: 'E',
+        controller: vulnerabilitiesModalCtrl,
+        replace: true,
+        scope: {
+            systemId: '<'
+        }
+    };
+}
+
+componentsModule.directive('vulnerabilitiesModal', vulnerabilitiesModal);
