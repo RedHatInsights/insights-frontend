32111dea1ae480a4687b5c13cba8714ae8f83192
Josh Kinlaw <jkinlaw@redhat.com>
2018-01-16 16:14:18 -0500

    hooks up vulnerabilities page

--- app/js/constants/vmaas.js
+++ app/js/constants/vmaas.js
@@ -1,6 +1,46 @@
 'use strict';
 var constantsModule = require('./');
 
+constantsModule.constant('VMAAS_PACKAGES_NOCVE', [{
+    id: 0,
+    name: 'ntp',
+    release_date: '2017-01-01',
+    critical_count: 0,
+    important_count: 0,
+    moderate_count: 1,
+    low_count: 0,
+    version: '4.0',
+    rhsas: [{
+        id: 'RHSA-2017:3071',
+        type: 'Security Advisory',
+        synopsis: 'ntp security update',
+        severity: 'moderate',
+        systems_affected: 1,
+        summary: 'An update for ntp is now available for Red Hat Enterprise Linux ' +
+                 '6.<br/>Red Hat Product Security has rated this update as having ' +
+                 'a security impact of Moderate. A Common Vulnerability Scoring ' +
+                 'System (CVSS) base score, which gives a detailed severity ' +
+                 'rating, is available for each vulnerability from the CVE ' +
+                 'link(s) in the References section.',
+        description: 'An update for ntp is now available for Red Hat Enterprise ' +
+                 'Linux 6.<br/>Red Hat Product Security has rated this update as ' +
+                 'Scoring having a security impact of Moderate. A Common ' +
+                 'Vulnerability System (CVSS) base score, which gives a detailed ' +
+                 'severity rating, is available for each vulnerability from the ' +
+                 'CVE link(s) in the References section.',
+        solution: 'https://access.redhat.com/articles/11258',
+        systems: [{
+            system_id: 'kinlaw-rhel-vm',
+            system_type_id: 105,
+            toString: 'kinlaw-rhel-vm',
+            account_number: '540155',
+            report_count: 2,
+            system_type: 'RHEL',
+            last_check_in: '2018-01-08T17:51:18:000Z'
+        }]
+    }]
+}]);
+
 constantsModule.constant('VMAAS_PACKAGES', [{
     id: 0,
     name: 'ntp',
@@ -15,6 +55,7 @@ constantsModule.constant('VMAAS_PACKAGES', [{
         type: 'Security Advisory',
         synopsis: 'ntp security update',
         severity: 'moderate',
+        systems_affected: 1,
         summary: 'An update for ntp is now available for Red Hat Enterprise Linux ' +
                  '6.<br/>Red Hat Product Security has rated this update as having ' +
                  'a security impact of Moderate. A Common Vulnerability Scoring ' +
--- app/js/states/vulnerabilities/vulnerabilities.controller.js
+++ app/js/states/vulnerabilities/vulnerabilities.controller.js
@@ -5,8 +5,70 @@ var statesModule = require('../');
 
 /**
  * @ngInject
+ *
+ * Currently uses mock data
  */
-function VulnerabilitiesCtrl() {
+function VulnerabilitiesCtrl($filter,
+                             $location,
+                             $scope,
+                             InventoryService,
+                             Utils,
+                             Vulnerability) {
+
+    $scope.pager = new Utils.Pager();
+    $scope.searchText = $location.search().searchText;
+    $scope.vulnerabilities = [];
+
+    $scope.sorter = new Utils.Sorter({
+        predicate: 'name',
+        reverse: false},
+        order);
+
+    /*
+     * Queries GET:/vulnerabilities and populates table data
+     */
+    function getData () {
+        $scope.loading = true;
+        let params = [];
+        params.search_term = $scope.searchText;
+
+        Vulnerability.getAll(params).then((vulnerabilities) => {
+            $scope.allVulnerabilities = vulnerabilities;
+            order();
+            $scope.loading = false;
+        });
+    }
+
+    function setVulnerabilities() {
+        let page = $scope.pager.currentPage - 1;
+        let pageSize = $scope.pager.perPage;
+        let offset = page * pageSize;
+        let arrayEnd = offset + pageSize < $scope.allVulnerabilities.total ?
+                offset + pageSize : $scope.allVulnerabilities.total;
+
+        $scope.vulnerabilities = $scope.allVulnerabilities.slice(offset, arrayEnd);
+    }
+
+    function reloadTable () {
+        $scope.pager.currentPage = 1;
+        setVulnerabilities();
+    }
+
+    function order () {
+        $scope.allVulnerabilities.resources = $filter('orderBy')(
+            $scope.allVulnerabilities.resources,
+            [($scope.sorter.reverse ?
+                '-' + $scope.sorter.predicate :
+                $scope.sorter.predicate)]);
+
+        reloadTable();
+    }
+
+    $scope.showSystem = function (system_id) {
+        InventoryService.showSystemModal({system_id: system_id}, true, 'vulnerability');
+    };
+
+    getData();
 }
 
 statesModule.controller('VulnerabilitiesCtrl', VulnerabilitiesCtrl);
