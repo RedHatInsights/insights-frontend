07c6f60b37c27b8c54ff2a8b07ff49a3a0913dd1
Jozef Hartinger <jharting@redhat.com>
2018-01-18 10:28:59 +0100

    remove unnecessary API call

--- app/js/components/incident/incidentLite/incidentLite.directive.js
+++ app/js/components/incident/incidentLite/incidentLite.directive.js
@@ -3,32 +3,18 @@
 
 const componentsModule = require('../../');
 
-function IncidentLiteController ($q, $scope, IncidentsService, Stats) {
+function IncidentLiteController ($q, $scope, IncidentsService) {
 
     function loadData () {
-        let promises = [];
-
         $scope.loading = true;
 
-        promises.push(loadIncidentNumbers());
-        promises.push(loadStats());
-
-        $q.all(promises).finally(() => {
-            $scope.loading = false;
-        });
+        loadIncidentNumbers().finally(() => $scope.loading = false);
     }
 
     // loads the total number of rule hits that are incidents
     function loadIncidentNumbers () {
         return IncidentsService.loadIncidents().then(() => {
             $scope.incidentCount = IncidentsService.incidentRulesWithHitsCount;
-        });
-    }
-
-    // loads the total number of rules that have hits
-    function loadStats () {
-        return Stats.getRules({include: 'ansible'}).then(function (res) {
-            $scope.totalHits = res.data.total;
         });
     }
 
