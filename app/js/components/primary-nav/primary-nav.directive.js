3e3a41fb11192e8e00f188aca955ef4b4db56d7b
Ian Page Hands <iphands@gmail.com>
2018-01-26 10:00:06 -0500

    Bad GitHub merge?

--- app/js/components/primary-nav/primary-nav.directive.js
+++ app/js/components/primary-nav/primary-nav.directive.js
@@ -7,9 +7,9 @@ var componentsModule = require('../');
 * @ngInject
 */
 
-function primaryNavCtrl($scope, Utils, $state, InsightsConfig, User) {
+function primaryNavCtrl($scope, Utils, $state, InsightsConfig, User, InsightsJwt) {
 
-  const policyAccounts = {
+    const policyAccounts = {
         540155: true,
         540694: true
     };
