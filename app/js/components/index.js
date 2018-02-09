32385f9c5ab0f68400e9bbbdecfb81c643f4a56c
Ian Page Hands <iphands@gmail.com>
2018-02-09 16:05:41 -0500

    lint

--- app/js/components/index.js
+++ app/js/components/index.js
@@ -1,7 +1,7 @@
 /*global require, module, __dirname, angular, process*/
 'use strict';
 
-if (process && process.env && process.env.NODE_ENV && process.env.NODE_ENV !== 'test' ) {
+if (process && process.env && process.env.NODE_ENV && process.env.NODE_ENV !== 'test') {
     const bulk = require('bulk-require');
     module.exports = angular.module('insights.components', []);
     bulk(__dirname, ['./**/!(*.spec).js']);
