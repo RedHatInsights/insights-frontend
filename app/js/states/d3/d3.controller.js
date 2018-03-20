4e5b1995880d908a3c8bcbc313bfbbead6422dd0
Jordan Eudy <jeudy@redhat.com>
2018-03-20 13:55:04 -0400

    original d3 map

--- app/js/states/d3/d3.controller.js
+++ app/js/states/d3/d3.controller.js
@@ -2,263 +2,123 @@
 
 const statesModule = require('../');
 const Datamaps = require('datamaps');
+const topojson = require('topojson');
 const d3 = require('d3');
 const $ = require('jquery');
+const world = require("../../../../node_modules/world-atlas/world/110m.json");
 
-let lastx, lasty;
-let dm;
-
-function Zoom(args) {
-  $.extend(this, {
-    scale:      { max: 50, currentShift: 0 },
-    $container: args.$container,
-    datamap:    args.datamap
-  });
-}
-
-Zoom.prototype.init = function() {
-    console.log(dm);
-    var paths = this.datamap.svg.selectAll("path"),
-        subunits = this.datamap.svg.selectAll(".datamaps-subunit");
-
-    // preserve stroke thickness
-    paths.style("vector-effect", "non-scaling-stroke");
-
-    subunits.call(
-        d3.behavior.drag().on("dragstart", function() {
-            d3.event.sourceEvent.stopPropagation();
-        })
-    );
-
-    const drag = d3.behavior.drag()
-        .on('dragstart', function () {
-            const sourceEvent = d3.event.sourceEvent;
-            lastx = sourceEvent.x;
-            lasty = sourceEvent.y;
-        })
-        .on('drag', () => {
-            const sourceEvent = d3.event.sourceEvent;
-            const currentTrans = dm.projection.translate();
-            const dx = sourceEvent.x - lastx;
-            const dy = sourceEvent.y - lasty;
-
-            dm.projection.translate([
-                currentTrans[0] + dx,
-                currentTrans[1] + dy
-            ]);
-
-            dm.svg.selectAll("path")
-                .attr('d', dm.path);
-
-            lastx = sourceEvent.x;
-            lasty = sourceEvent.y;
-        });
-
-    subunits.call(drag);
-    dm.svg.call(drag);
-
-    this.scale.set = this._getScalesArray();
-    this.d3Zoom = d3.behavior.zoom().scaleExtent([ 1, this.scale.max ]);
-  // console.log(d3.behavior);
-  // // map drag capability
-  // d3.behavior.drag().on('drag', function() {
-  //   var dx = d3.event.dx;
-  //   var dy = d3.event.dy;
-
-  //   console.log('being dragged');
-  // })
-
-      this._displayPercentage(1);
-      this.listen();
-};
-
-Zoom.prototype.listen = function() {
-    //this.$buttons.off("click").on("click", this._handleClick.bind(this));
-
-    this.datamap.svg
-        .call(this.d3Zoom.on("zoom", this._handleScroll.bind(this)))
-        .on("dblclick.zoom", null); // disable zoom on double-click
-};
-
-Zoom.prototype.reset = function() {
-    this._shift("reset");
-};
-
-Zoom.prototype._handleScroll = function() {
-    var translate = d3.event.translate,
-        scale = d3.event.scale,
-        limited = this._bound(translate, scale);
-
-    this.scrolled = true;
-
-    this._update(limited.translate, limited.scale);
-};
-
-Zoom.prototype._handleClick = function(event) {
-    var direction = $(event.target).data("zoom");
-
-    this._shift(direction);
-};
-
-Zoom.prototype._shift = function(direction) {
-    var center = [ this.$container.width() / 2, this.$container.height() / 2 ],
-        translate = this.d3Zoom.translate(), translate0 = [], l = [],
-        view = {
-            x: translate[0],
-            y: translate[1],
-            k: this.d3Zoom.scale()
-        }, bounded;
-
-    translate0 = [
-        (center[0] - view.x) / view.k,
-        (center[1] - view.y) / view.k
-    ];
-
-    if (direction == "reset") {
-        view.k = 1;
-        this.scrolled = true;
-    } else {
-        view.k = this._getNextScale(direction);
-    }
-
-    l = [ translate0[0] * view.k + view.x, translate0[1] * view.k + view.y ];
-
-    view.x += center[0] - l[0];
-    view.y += center[1] - l[1];
-
-    bounded = this._bound([ view.x, view.y ], view.k);
-
-    this._animate(bounded.translate, bounded.scale);
-};
-
-Zoom.prototype._bound = function(translate, scale) {
-    var width = this.$container.width(),
-        height = this.$container.height();
-
-    translate[0] = Math.min(
-        (width / height)  * (scale - 1),
-        Math.max( width * (1 - scale), translate[0] )
-    );
-
-    translate[1] = Math.min(0, Math.max(height * (1 - scale), translate[1]));
-
-    return { translate: translate, scale: scale };
-};
-
-Zoom.prototype._update = function(translate, scale) {
-    this.d3Zoom
-        .translate(translate)
-        .scale(scale);
-
-    this.datamap.svg.selectAll("g")
-        .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
-
-    this._displayPercentage(scale);
-};
-
-Zoom.prototype._animate = function(translate, scale) {
-    var _this = this,
-        d3Zoom = this.d3Zoom;
-
-    d3.transition().duration(350).tween("zoom", function() {
-        var iTranslate = d3.interpolate(d3Zoom.translate(), translate),
-            iScale = d3.interpolate(d3Zoom.scale(), scale);
-
-        return function(t) {
-            _this._update(iTranslate(t), iScale(t));
-        };
-    });
-};
-
-Zoom.prototype._displayPercentage = function(scale) {
-    var value;
-
-    value = Math.round(Math.log(scale) / Math.log(this.scale.max) * 100);
-    //console.log('zoom percent: ', `${value}%`);
-    //this.$info.text(value + "%");
-};
-
-Zoom.prototype._getScalesArray = function() {
-    var array = [],
-        scaleMaxLog = Math.log(this.scale.max);
-
-    for (var i = 0; i <= 10; i++) {
-        array.push(Math.pow(Math.E, 0.1 * i * scaleMaxLog));
-    }
-
-    return array;
-};
-
-Zoom.prototype._getNextScale = function(direction) {
-    var scaleSet = this.scale.set,
-        currentScale = this.d3Zoom.scale(),
-        lastShift = scaleSet.length - 1,
-        shift, temp = [];
-
-    if (this.scrolled) {
-
-        for (shift = 0; shift <= lastShift; shift++) {
-            temp.push(Math.abs(scaleSet[shift] - currentScale));
-        }
-
-        shift = temp.indexOf(Math.min.apply(null, temp));
-
-        if (currentScale >= scaleSet[shift] && shift < lastShift) {
-            shift++;
-        }
-
-        if (direction == "out" && shift > 0) {
-            shift--;
-        }
-
-        this.scrolled = false;
-
-    } else {
-
-        shift = this.scale.currentShift;
-
-        if (direction == "out") {
-            shift > 0 && shift--;
-        } else {
-            shift < lastShift && shift++;
-        }
-    }
-
-    this.scale.currentShift = shift;
-
-    return scaleSet[shift];
-};
-
-function Datamap() {
-    this.$container = $("#d3");
-    this.hello = 'hello';
-    this.instance = new Datamaps({
-        scope: 'world',
-        element: this.$container.get(0),
-        projection: 'mercator',
-        done: this._handleMapReady.bind(this),
-        responsive: true
-    });
-
-    this.instance.path = d3.geo.path().projection(this.instance.projection);
-    dm = this.instance;
-
-    const inst = this.instance;
-    window.addEventListener('resize', function() {
-        inst.resize();
-    });
-}
-
-Datamap.prototype._handleMapReady = function(datamap) {
-    this.zoom = new Zoom({
-        $container: this.$container,
-        datamap: datamap
-    });
-}
+// returns the angle in degrees between two points on the map
+// used for the x tranlation for infinite scroll
+const getAngle = (p1, p2) => (Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180) / Math.PI;
 
 function D3Ctrl() {
-    const map = new Datamap();
-    map.zoom.init();
+    const div = $('#d3').get()[0];
+    // const dm = new Datamaps({
+    //     scope: 'world',
+    //     element: div,
+    //     projection: 'mercator',
+    //     responsive: true
+    // });
+
+    // d3.select(window).on("resize", dm.resize);
+
+    // dm.path = d3.geo.path().projection(dm.projection);
+    // dm.paths = dm.svg.selectAll("path");
+    // dm.dm_subunits = dm.svg.selectAll(".datamaps-subunit");
+    // dm.paths.style("vector-effect", "non-scaling-stroke");
+
+    var width = div.scrollWidth,
+        height = div.scrollHeight,
+        rotate = 0,        // so that [-60, 0] becomes initial center of projection
+        maxlat = 79;        // clip northern and southern poles (infinite in mercator)
+
+    var projection = d3.geo.mercator()
+        .rotate([rotate, 0])
+        .scale(1)           // we'll scale up to match viewport shortly.
+        .translate([width / 2, height / 2]);
+
+    // find the top left and bottom right of current projection
+    function mercatorBounds(projection, maxlat) {
+        var yaw = projection.rotate()[0],
+            xymax = projection([-yaw+180-1e-6,-maxlat]),
+            xymin = projection([-yaw-180+1e-6, maxlat]);
+
+        return [xymin,xymax];
+    }
+
+    // set up the scale extent and initial scale for the projection
+    var b = mercatorBounds(projection, maxlat),
+        s = width/(b[1][0]-b[0][0]),
+        scaleExtent = [s, 15*s];
+
+    projection
+        .scale(scaleExtent[0]);
+
+    var zoom = d3.behavior.zoom()
+        .scaleExtent(scaleExtent)
+        .scale(projection.scale())
+        .translate([0,0])               // not linked directly to projection
+        .on("zoom", redraw);
+
+    var path = d3.geo.path()
+        .projection(projection);
+
+    var svg = d3.selectAll('.d3')
+        .append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .call(zoom);
+
+    svg.selectAll('path')
+            .data(topojson.feature(world, world.objects.countries).features)
+            .enter().append('path')
+
+    redraw();
+
+    // track last translation and scale event we processed
+    let tlast = [0,0],
+        slast = null,
+        angle = null;
+
+    function redraw() {
+        if (d3.event) {
+            const coords = d3.mouse(this);
+            var scale = d3.event.scale,
+                t = d3.event.translate;
+
+            let dx = t[0] - tlast[0],
+                dy = t[1] - tlast[1],
+                yaw = projection.rotate()[0],
+                tp = projection.translate();
+
+            // if scaling changes, ignore translation (otherwise touch zooms are weird)
+            if (scale != slast) {
+                projection.scale(scale);
+                projection.translate(tp);
+            } else {
+                console.log((((360.0 * dx) / width) * scaleExtent[0]) / scale);
+                // use x translation to rotate based on current scale
+                projection.rotate([yaw + (((360.0 * dx) / width) * scaleExtent[0]) / scale, 0, 0]);
+                // use y translation to translate projection, clamped by min/max
+                let b = mercatorBounds(projection, maxlat);
+                if (b[0][1] + dy > 0) dy = -b[0][1];
+                else if (b[1][1] + dy < height) dy = height - b[1][1];
+                projection.translate([tp[0], tp[1] + dy]);
+            }
+            // save last values.  resetting zoom.translate() and scale() would
+            // seem equivalent but doesn't seem to work reliably?
+            slast = scale;
+            tlast = t;
+        }
+
+        svg.selectAll('path')
+            .attr('d', path);
+    }
+
+    window.addEventListener('keypress', () => {
+        // console.log(projection.scale());
+        // console.log(projection.translate());
+    });
 }
 
 statesModule.controller('D3Ctrl', D3Ctrl);
