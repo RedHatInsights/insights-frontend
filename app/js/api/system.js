9dac8630876d908b94848d53c6c0b150e79df8b4
Ian Page Hands <iphands@gmail.com>
2018-03-21 11:25:41 -0400

    more faking policies

--- app/js/api/system.js
+++ app/js/api/system.js
@@ -7,31 +7,32 @@ const URI = require('urijs');
 
 const getDemoPolicies = (systemId) => {
     return {
-        total: 1,
-        resources: [
-            {
-                created_at: '2018-02-02T16:49:38.000Z',
-                updated_at: '2018-02-02T16:49:38.000Z',
-                account_number: '6',
-                system_id: systemId,
-                policy_id: 'fips-mode-check',
-                policy_name: 'fips-mode-check',
-                raw_output: '',
-                checks_pass: 0,
-                checks_fail: 1,
-                checks_error: 0,
-                check_results: [
-                    {
-                        name: 'fips mode must be enabled',
-                        result: 'fail'
-                    }
-                ]
-            }
-        ],
-        policies_pass: 0,
-        policies_fail: 1,
-        policies_error: 0
-    };
+        data: {
+            total: 1,
+            resources: [
+                {
+                    created_at: '2018-02-02T16:49:38.000Z',
+                    updated_at: '2018-02-02T16:49:38.000Z',
+                    account_number: '6',
+                    system_id: systemId,
+                    policy_id: 'fips-mode-check',
+                    policy_name: 'fips-mode-check',
+                    raw_output: '',
+                    checks_pass: 0,
+                    checks_fail: 1,
+                    checks_error: 0,
+                    check_results: [
+                        {
+                            name: 'fips mode must be enabled',
+                            result: 'fail'
+                        }
+                    ]
+                }
+            ],
+            policies_pass: 0,
+            policies_fail: 1,
+            policies_error: 0
+        }};
 };
 
 const demoSystemTypes = [
@@ -363,10 +364,8 @@ function System(
 
         getSystemPolicies (systemId) {
             return {
-                then: () => {
-                    return {
-                        data: getDemoPolicies(systemId)
-                    };
+                then: (cb) => {
+                    cb(getDemoPolicies(systemId));
                 }
             };
 
