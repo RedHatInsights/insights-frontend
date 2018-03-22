08e12898cc96487c4aee841ecb2538ac98b93a7f
Jordan Eudy <jeudy@redhat.com>
2018-03-22 16:16:27 -0400

    added pins to map

--- app/js/states/pmaas/map/dashboardMap.controller.js
+++ app/js/states/pmaas/map/dashboardMap.controller.js
@@ -4,8 +4,14 @@
 const statesModule = require('../../');
 const d3 = require('d3');
 const topojson = require('topojson');
-const world = require('../../../../../node_modules/world-atlas/world/110m.json');
-const priv = {};
+const world = require('./dashboardMap.json');
+const priv = {
+    tlast: [0, 0],
+    slast: null,
+    pins: []
+};
+
+console.log(world);
 
 // returns the angle in degrees between two points on the map
 // used for the x tranlation for infinite scroll
@@ -25,7 +31,7 @@ priv.getConf = () => {
         width: div.scrollWidth,
         height: div.scrollHeight,
         rotate: 0,
-        maxLatitude: 79
+        maxLatitude: 86
     };
 
     console.log(`getConf: ${JSON.stringify(ret, false, 2)}`);
@@ -34,102 +40,126 @@ priv.getConf = () => {
 
 priv.getScale = (projection, maxLatitude, width) => {
     // set up the scale extent and initial scale for the projection
-    const bounds = priv.mercatorBounds(projection, maxLatitude);
+    const bounds = priv.mercatorBounds(priv.projection, maxLatitude);
+    console.log(bounds);
     const scale  = width / (bounds[1][0] - bounds[0][0]);
     return [scale, 15 * scale];
 };
 
-function DashboardMapCtrl() {
-    let conf = priv.getConf();
-
-    window.priv = priv;
-
-    window.addEventListener('keypress', () => {
-        // console.log(projection.scale());
-        // console.log(projection.translate());
-    });
-
-    let projection = d3.geo.mercator()
+priv.reInit = (conf) => {
+    priv.projection
         .rotate([conf.rotate, 0])
         .scale(1)
         .translate([conf.width / 2, conf.height / 2]);
 
-    let scaleExtent = priv.getScale(projection, conf.maxLatitude, conf.width);
+    priv.zoom
+        .scale(priv.projection.scale())
+        .translate([0, 0]);
 
-    projection.scale(scaleExtent[0]);
+    priv.svg
+        .attr('width', conf.width)
+        .attr('height', conf.height);
 
-    let zoom = d3.behavior.zoom()
-        .scaleExtent(scaleExtent)
-        .scale(projection.scale())
+    priv.redraw();
+};
+
+priv.init = (conf) => {
+    priv.projection = d3.geo.mercator()
+        .rotate([conf.rotate, 0])
+        .scale(1)
+        .translate([conf.width / 2, (conf.height / 2) + 100]);
+
+    priv.scaleExtent = priv.getScale(priv.projection, conf.maxLatitude, conf.width);
+
+    priv.projection.scale(priv.scaleExtent[0]);
+
+    priv.zoom = d3.behavior.zoom()
+        .scaleExtent(priv.scaleExtent)
+        .scale(priv.projection.scale())
         .translate([0, 0]) // not linked directly to projection
-        .on('zoom', redraw);
+        .on('zoom', priv.redraw);
 
-    let path = d3.geo.path()
-        .projection(projection);
+    priv.path = d3.geo.path()
+        .projection(priv.projection);
 
-    let svg = d3.select('.map')
+    priv.svg = d3.select('.map')
         .append('svg')
         .attr('width', conf.width)
         .attr('height', conf.height)
-        .call(zoom);
+        .call(priv.zoom);
 
-    svg.selectAll('path')
-            .data(topojson.feature(world, world.objects.countries).features)
-            .enter().append('path');
+    priv.centroids = priv.svg.append('g')
+        .attr('class', 'centroid');
 
-    // track last translation and scale event we processed
-    let tlast = [0,0];
-    let slast = null;
+    priv.svg.selectAll('path')
+        .data(topojson.feature(world, world.objects.countries).features)
+        .enter().append('path')
+        .attr('d', (d) => {
+            if (d.id === 'USA' || d.id === 'CAN') {
+                const pin = priv.centroids.append('circle')
+                    .attr('data-toggle-target', d.id)
+                    .attr('r', '8px')
+                    .attr('fill', 'red')
+                    .style('display', 'inline')
+                    .attr('transform', 'translate(' + priv.path.centroid(d) + ')');
 
-    function redraw() {
-        if (d3.event) {
-            let scale = d3.event.scale;
-            let t = d3.event.translate;
+                priv.pins.push({
+                    pin: pin,
+                    pinID: d.id
+                });
+            }
+        });
 
-            let dx = t[0] - tlast[0];
-            let dy = t[1] - tlast[1];
-            let yaw = projection.rotate()[0];
-            let tp = projection.translate();
+    priv.redraw();
+};
 
-            // if scaling changes, ignore translation (otherwise touch zooms are weird)
-            if (scale !== slast) {
-                projection.scale(scale);
-                projection.translate(tp);
-            } else {
-                const angle = (((360.0 * dx) / conf.width) * scaleExtent[0]) / scale;
+priv.redraw = () => {
+    const conf = priv.getConf();
 
-                // use x translation to rotate based on current scale
-                projection.rotate([yaw + angle, 0, 0]);
+    if (d3.event) {
+        let scale = d3.event.scale;
+        let t = d3.event.translate;
 
-                // use y translation to translate projection, clamped by min/max
-                let b = priv.mercatorBounds(projection, conf.maxlat);
+        let dx = t[0] - priv.tlast[0];
+        let dy = t[1] - priv.tlast[1];
+        let yaw = priv.projection.rotate()[0];
 
-                if (b[0][1] + dy > 0) {
-                    dy = -b[0][1];
-                } else if (b[1][1] + dy < conf.height) {
-                    dy = conf.height - b[1][1];
-                }
+        // if scaling changes, ignore translation (otherwise touch zooms are weird)
+        if (scale !== priv.slast) {
+            priv.projection.scale(scale);
+        } else {
+            const angle = (((360.0 * dx) / conf.width) * priv.scaleExtent[0]) / scale;
 
-                projection.translate([tp[0], tp[1] + dy]);
+            // use x translation to rotate based on current scale
+            priv.projection.rotate([yaw + angle, 0, 0]);
+
+            // use y translation to translate projection, clamped by min/max
+            let b = priv.mercatorBounds(priv.projection, conf.maxLatitude);
+
+            if (b[0][1] + dy > 0) {
+                dy = -b[0][1];
+            } else if (b[1][1] + dy < conf.height) {
+                dy = conf.height - b[1][1];
             }
 
-            // save last values.  resetting zoom.translate() and scale() would
-            // seem equivalent but doesn't seem to work reliably?
-            slast = scale;
-            tlast = t;
+            //priv.projection.translate([tp[0], tp[1] + dy]);
         }
 
-        svg.selectAll('path').attr('d', path);
+        // save last values.  resetting zoom.translate() and scale() would
+        // seem equivalent but doesn't seem to work reliably?
+        priv.slast = scale;
+        priv.tlast = t;
     }
 
-    window.onresize = () => {
-        conf = priv.getConf();
-        svg.attr('width', conf.width);
-        svg.attr('height', conf.height);
-        redraw();
-    };
+    priv.svg.selectAll('path').attr('d', priv.path);
+};
 
-    redraw();
+function DashboardMapCtrl($timeout) {
+    $timeout(() => {
+        priv.init(priv.getConf());
+    }, 0.25);
+
+    window.onresize = () => priv.reInit(priv.getConf());
 }
 
 statesModule.controller('DashboardMapCtrl', DashboardMapCtrl);
