99ecce86d5391765923362632e8e2bf05656af7e
Chris Chase <cchase@redhat.com>
2018-03-26 17:15:23 -0400

    Minor fixes to recommendation text/style.

--- app/js/demoData/recommendations.js
+++ app/js/demoData/recommendations.js
@@ -24,7 +24,7 @@ pub.getRecommendations = (accountNumber, systemId) => {
                 confidence: 4,
                 likelihood: 3,
                 reliability: 3,
-                reason: '<p>This system is running on OpenStack Platform 10. and Neutron network service. Red Hat analysed that <strong>93%</strong> of our customers running a similar deployment, have configured the MTU of the tenant network to 1450.</p><p>This systems configuration of the MTU of the tenant network is currently configured to be 950</p><p>Parameter Value is used in 0.75% of the similar deployments \nParameter Value is in the 2σ interval\n</p><p>Predictive accuracy of a model is <strong>very high.</strong></p>',
+                reason: '<p>This system is running on OpenStack Platform 10. and Neutron network service. Red Hat analysed that <strong>93%</strong> of our customers running a similar deployment, have configured the MTU of the tenant network to 1450.</p><p>This systems configuration of the MTU of the tenant network is currently configured to be 950</p><p>Parameter Value is used in 0.75% of the similar deployments.  Parameter Value is in the 2σ interval</p><p>Predictive accuracy of a model is <strong>very high.</strong></p>',
                 resolution: '<p>Red Hat recommends that you change <code>global_physnet_mtu</code> setting in <code>/etc/neutron/neutron.conf</code>  file to fix this issue.</p>'
             }
         },
